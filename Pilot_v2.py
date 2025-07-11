69c6178752d217fd03f0a45d11a7c2a41e5ee2c6
SkhumbuzoT <109624354+SkhumbuzoT@users.noreply.github.com>
2025-07-11 13:11:17 +0200

    Update Pilot_v2.py

--- Pilot_v2.py
+++ Pilot_v2.py
@@ -45,7 +45,19 @@ COLOR_MAP = {
 
 @st.cache_resource
 def get_gsheet_client():
-    creds_dict = st.secrets["gcp_service_account"]
+    # Create credentials from secrets
+    creds_dict = {
+        "type": st.secrets["gcp_service_account"]["type"],
+        "project_id": st.secrets["gcp_service_account"]["project_id"],
+        "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+        "private_key": st.secrets["gcp_service_account"]["private_key"],
+        "client_email": st.secrets["gcp_service_account"]["client_email"],
+        "client_id": st.secrets["gcp_service_account"]["client_id"],
+        "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+        "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
+    }
     creds = service_account.Credentials.from_service_account_info(creds_dict)
     return gspread.authorize(creds)
 
